8ade653694ac33531c63cb34c32c852390002877
nikhilrautan <nikhilrautan1@gmail.com>
2026-02-01 20:18:50 +0530

    Update OBJECTS

--- 03_ JS/7.OBJECTS/index.js
+++ 03_ JS/7.OBJECTS/index.js
@@ -1,179 +1,149 @@
-// object
-// key value
-// const user = {
-//    name: "Rohit",
-//    age: 20,
-//    emailId: "negi@gmail.com",
-//    amount: 3400,
-//    "home address": "dwarka"
-// }
+/*
+JavaScript Objects
 
+Object = key-value pair
+Used to store related data together.
+*/
 
-// console.log(user["name"]);
-// console.log(user.age);
-// console.log(user["age"]);
-// console.log(user["home address"]);
-// console.log(typeof user);
-// CRUD operation: Create read update delete
+// 1) Creating an object
+const user = {
+  name: "Nikhil",
+  age: 20,
+  emailId: "nikhil@gmail.com",
+  amount: 3400,
+  "home address": "Chaukhutia"
+};
 
+// 2) Accessing values
+console.log(user["name"]);
+console.log(user.age);
+console.log(user["age"]);
+console.log(user["home address"]);
+console.log(typeof user);
 
-// console.log(user.age);
+// CRUD operations
+// Create / Update
+user.aadhar = 1234;      // new key
+user.amount = 5000;      // update existing key
+console.log(user);
 
-//  update
-// user.aadhar = 1234;
-// user.amount = 5000;
+// Delete
+delete user.emailId;
+console.log(user);
 
-// console.log(user);
+// 3) Copy by reference
+const user2 = user;      // points to same object
+user2.age = 90;
 
-// // delete
-// delete user.emailId;
-// console.log(user);
+console.log("user:", user);
+console.log("user2:", user2);
 
+// 4) Object methods
+console.log(Object.keys(user));      // all keys
+console.log(Object.values(user));    // all values
+console.log(Object.entries(user));   // key-value pairs
 
-// const user = {
-//    name: "Rohit",
-//    age: 20,
-//    emailId: "negi@gmail.com",
-//    amount: 3400,
-// }
+// 5) Loop in object (for...in)
+for (let key in user) {
+  console.log(key, user[key]);
+}
 
-// const user2 = user;
-// user2.age = 90;
+// Loop using Object.keys()
+const tempKeys = Object.keys(user);
+for (let key of tempKeys) {
+  console.log(key);
+}
 
-// console.log(user);
+// Loop values
+for (let value of Object.values(user)) {
+  console.log(value);
+}
 
-// Important
+// Loop entries
+for (let [key, value] of Object.entries(user)) {
+  console.log(key, value);
+}
 
-// console.log(Object.keys(user));
-// console.log(Object.values(user));
-// console.log(Object.entries(user));
+// 6) Object destructuring
+const user3 = {
+  name: "Nikhil",
+  age: 20,
+  emailId: "nikhil@gmail.com",
+  amount: 3400
+};
 
-// for(let keys in user){
-//     console.log(keys, user[keys]);
-// }
+const { name: userName, age: userAge } = user3;
+console.log(userName, userAge);
 
-// user.keys user["name"] user["age"]
+// 7) Array destructuring
+const arr = [10, 20, 40, 90, 11];
+const [first, second] = arr;
+console.log(first, second);
 
+// 8) Function inside object (this keyword)
+const user4 = {
+  name: "Nikhil",
+  age: 20,
+  greeting: function () {
+    console.log(`Hello ${this.name}`);
+    return 20;
+  }
+};
 
-// const user = {
-//    name: "Rohit",
-//    age: 20,
-//    emailId: "negi@gmail.com",
-//    amount: 3400,
-// }
+const user5 = {
+  name: "Mohan",
+  account: 201,
+  greeting: function () {
+    console.log(`Strike is coming on 18 october ${this.name}`);
+    return 20;
+  }
+};
 
-// for of loop array ke
-// const temnparr = Object.keys(user);
-// console.log(temnparr)
-// for(let keys of temnparr){
-//     console.log(keys);
-// }
+// function call
+user5.greeting();
+const val = user4.greeting();
+console.log(val);
 
-// for(let values of Object.values(user)){
-//     console.log(values);
-// }
+// Assign function from one object to another
+user5.greeting = user4.greeting;
+user5.greeting();
 
-// console.log(Object.entries(user));
-// [
-//   [ 'name', 'Rohit' ],
-//   [ 'age', 20 ],
-//   [ 'emailId', 'negi@gmail.com' ],
-//   [ 'amount', 3400 ]
-// ]
+// 9) Nested object
+const user6 = {
+  name: "Nikhil",
+  age: 20,
+  emailId: "nikhil@gmail.com",
+  amount: 3400,
+  address: {
+    city: "Chaukhutia",
+    state: "Uttarkhand"
+  }
+};
 
-// for(let [keys,values] of Object.entries(user)){
-//     console.log(keys, values);
+// 10) Shallow copy
+const shallowCopy = { ...user6 };
+shallowCopy.name = "Mohan";
+shallowCopy.address.city = "Dwarka"; // this also changes original nested object
 
-// }
+console.log("shallowCopy:", shallowCopy);
+console.log("original user6:", user6);
 
+// 11) Deep copy
+const deepCopy = structuredClone(user6);
+deepCopy.address.city = "Delhi";
 
-// const name = user.name;
-// const age = user.age;
+console.log("deepCopy:", deepCopy);
+console.log("original user6:", user6);
 
-// object ko destructing karna
-// const {name:userName,age:userAge} = user;
+// 12) Symbol key in object
+const sym = Symbol("id");
 
-// const arr = [10,20,40,90,11];
+const user7 = {
+  name: "Nikhil",
+  age: 20,
+  0: 100,
+  2: "Mohan",
+  [sym]: "Hello Ji"
+};
 
-// const [first,second]= arr;
-
-// console.log(userName,userAge);
-// console.log(first,second);
-
-
-// const user = {
-//    name: "Rohit",
-//    age: 20,
-//    emailId: "negi@gmail.com",
-//    amount: 3400,
-//    greeting: function(){
-//     console.log(`Strike is coming on 18 october ${this.name}`);
-//     return 20;
-//    }
-// }
-
-// const user2 = {
-//     name:"Mohan",
-//     account:201,
-//      greeting: function(){
-//     console.log(`Strike is coming on 18 october ${this.name}`);
-//     return 20;
-//    }
-// }
-
-// user2.greeting = user.greeting;
-
-// user2.greeting();
-// const va = user.greeting();
-// console.log(va);
-
-// nested object
-
-// const user = {
-//    name: "Rohit",
-//    age: 20,
-//    emailId: "negi@gmail.com",
-//    amount: 3400,
-//    address: {
-//     city:"kotdwar",
-//     state:"Uttarkhand"
-//    }
-// }
-
-// shallow copy
-// const user2 = {...user};
-// user2.name = "Mohan";
-// user2.address.city = "Dwarka";
-
-// console.log(user2);
-
-// console.log(user.address.city);
-
-
-// deep copy
-
-// const user2 = structuredClone(user);
-// user2.address.city = "Dwarka";
-// console.log(user);
-// keY; String || Symbol
-
-// const sym = Symbol("id");
-
-// const user = {
-//     name:"Rohit",
-//     age:20,
-//     0: 100,
-//     2: "Mohan",
-//     [sym]:"Hello Ji"
-// }
-
-// const arr = [10,20,30,40];
-
-// {
-//     0:10,
-//     1:20,
-//     2:30,
-//     3: 40
-// }
-
-console.log(user[sym]);
+console.log(user7[sym]);
