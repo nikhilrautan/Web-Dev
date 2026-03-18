cea473fd3d82ff079d487f68cbeff8e8213e5ad0
nikhilrautan <nikhilrautan1@gmail.com>
2026-03-18 21:32:12 +0530

    React + vite Complete setup and Deployment

--- 04_React/03/app.js
+++
@@ -1,2 +0,0 @@
-const element  =<h1> Hello Coder army</h1>
-ReactDOM.createRoot(document.getElementById('root')).render(element);
---
+++ 04_React/03/app.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const element  =<h1> Hello Coder army</h1>
+ReactDOM.createRoot(document.getElementById('root')).render(element);
