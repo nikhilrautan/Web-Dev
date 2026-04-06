0a089c911ab13a5a83cd1c0f8c16c8c306b87145
nikhilrautan <nikhilrautan1@gmail.com>
2026-04-06 23:50:20 +0530

    UPDATED

--- 03_ JavaScript/03.CONDITIONALS, LOOPS, OPERATORS/03_null_undefined_nan.js
+++ 03_ JavaScript/03.CONDITIONALS, LOOPS, OPERATORS/03_null_undefined_nan.js
@@ -10,7 +10,6 @@ console.log(null == undefined);   // true
 console.log(null === undefined);  // false
 
 
-
 console.log(null == 0);       // false
 console.log(null == "");      // false
 console.log(null == false);   // false
