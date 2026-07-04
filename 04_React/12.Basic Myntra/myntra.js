7f9f24e37be46f3b5f7b31e5edd696585117686e
nikhilrautan <nikhilrautan1@gmail.com>
2026-07-04 10:49:34 +0530

    Update React folder

--- 04_React/12.Basic Myntra/myntra.js
+++ 04_React/12.Basic Myntra/myntra.js
@@ -28,14 +28,14 @@ function App(){
     // Header
     // Body
     <div style={{display:"flex", gap:"10px" , flexWrap:"wrap"}}>
-        {/* <Card cloth="T-shirt" offer="20-80%off" />
+        <Card cloth="T-shirt" offer="20-80%off" />
         <Card cloth="Jeans" offer="30-80%off"/>
         <Card cloth="pant" offer="50-60%off"/>
         <Card cloth="Kurta" offer="30-80%off"/>
         <Card cloth="Pajama" offer="10-80%off" />
         <Card cloth="T-shirt" offer="20-80%off"/>
         <Card cloth="T-shirt" offer="20-80%off"/>
-        <Card cloth="T-shirt" offer="20-80%off"/> */}
+        <Card cloth="T-shirt" offer="20-80%off"/>
 
         {
           arr.map((value,index)=> <Card key={index} cloth={value.cloth} offer={value.Offer}/>)   
