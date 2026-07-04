099cdd9331aff23c526d4f2c4718041aec719737
nikhilrautan <nikhilrautan1@gmail.com>
2026-07-04 14:51:10 +0530

    Update React folder

---
+++ 04_React/12.Basic Myntra/src/utils/dummy.js
@@ -0,0 +1,3 @@
+const arr = [{cloth:"Tshirt", Offer:"20-40%Off"},{cloth:"Pant", Offer:"30-50%Off"},{cloth:"Skirt", Offer:"10-20%Off"},{cloth:"Kurta", Offer:"30-60%Off"},{cloth:"Patloon", Offer:"11-40%Off"},{cloth:"Shoes", Offer:"40-60%Off"},{cloth:"Shirt", Offer:"10-20%Off"}]
+
+export default arr;
