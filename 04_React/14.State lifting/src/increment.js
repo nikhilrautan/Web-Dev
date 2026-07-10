fa8d94bcb5af388748c276fa50c89518442d6604
nikhilrautan <nikhilrautan1@gmail.com>
2026-07-11 00:50:48 +0530

    Update State Lifting folder

--- 04_React/14.State lifting/src/increment.js
+++ 04_React/14.State lifting/src/increment.js
@@ -5,7 +5,7 @@ export default function Increment({counts,setCounts}){
     return (
         <>
         <h2>Child Counter is :{counts}</h2>
-        <button>Increment</button>
+        <button onClick={()=>setCounts(counts+1)}>Increment</button>
         </>
     )
 }
