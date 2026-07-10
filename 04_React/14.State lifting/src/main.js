b420835ba37bb0af02decc59db96f7f2181c2498
nikhilrautan <nikhilrautan1@gmail.com>
2026-07-11 00:55:23 +0530

    Update State Lifting folder

--- 04_React/14.State lifting/src/main.js
+++ 04_React/14.State lifting/src/main.js
@@ -1,20 +1,23 @@
-import React, { useState , useMemo, useCallback, useEffect} from "react";
-import reactDOM from "react-dom/client"
-import Increment from "./increment"
+import React, { useState, useMemo, useCallback, useEffect } from "react";
+import ReactDOM from "react-dom/client"
+import Increment from "./Increment"
+import Decrement from "./Decrement"
 
 function App(){
-    const [count,setCount] = useState(0);
 
-    return (
-        <>
-      <h1>Parent Counter is :{count}</h1>
-      <Increment counts={count} setCounts= {setCount}/>
+  const [count,setCount] = useState(0);
 
-        </>
-    )
+  return (
+    <>
+    <h1>Hello Coder Army</h1>
+    <Increment counts = {count} setCounts={setCount}/>
+    <Decrement counts = {count} setCounts={setCount}/>
+    </>
+  )
 }
 
 
 
 
-reactDOM.createRoot(document.getElementById('root')).render(<App></App>);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App></App>);
