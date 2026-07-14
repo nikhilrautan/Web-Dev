0a30ac743711dcd24ea38d614ccc0fa266227249
nikhilrautan <nikhilrautan1@gmail.com>
2026-07-14 22:02:05 +0530

    Update React Router

--- 04_React/10.React_Router/src/Details.js
+++ 04_React/10.React_Router/src/Details.js
@@ -2,13 +2,14 @@ import { Outlet,Link } from "react-router"
 
 export default function Details(){
 
+    
     return(
         <>
         <nav>
             <Link to="/Details">Details</Link>
             <Link to="Hello">Hello</Link>
             <Link to="Hi">Hi</Link>
-            
+
         </nav>
 
         <Outlet></Outlet>
