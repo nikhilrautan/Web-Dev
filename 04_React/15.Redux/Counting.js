86e9fb1b2365947d7bf4bf16e8ad5adeb3ba3d79
nikhilrautan <nikhilrautan1@gmail.com>
2026-07-15 10:44:33 +0530

    Update React redux

--- 04_React/15.Redux/Counting.js
+++ 04_React/15.Redux/Counting.js
@@ -1,10 +1,15 @@
 import { useDispatch, useSelector } from "react-redux"
+import { Increment, Decrement, Reset } from "./Slicer1";
+
 
 export default function Counting(){
- 
+     
+    const count = useSelector((state)=> state.slice1.count);
+    const dispatch = useDispatch();
 
-    const count = useSelector((state)=>state.slice.slice1.count);
-   const dispatch =  useDispatch();
+    console.log(Increment());
+    console.log(Decrement());
+
     return(
         <>
         <h1>Counter is {count}</h1>
--- 04_React/15.Redux/Stores.js
+++ 04_React/15.Redux/Stores.js
@@ -2,10 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import slice1Reducer from "./Slicer1"
 
 const stores = configureStore({
-    reducer :{
-    slice1 : slice1Reducer,
-    slice2 : slice2Reducer,
+    reducer:{
+      slice1: slice1Reducer,
     }
 })
 
 export default stores;
+
+// slice name: Reducer
