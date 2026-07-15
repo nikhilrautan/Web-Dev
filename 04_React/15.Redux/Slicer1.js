0a47034f80ddc57321cf2d608c2d2e4c71addea8
nikhilrautan <nikhilrautan1@gmail.com>
2026-07-15 10:02:50 +0530

    Update React Redux

--- 04_React/15.Redux/Slicer1.js
+++ 04_React/15.Redux/Slicer1.js
@@ -3,6 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const reactslicer = createSlice(
     {
       name: "slice1",
-      initialState: {count:0}
-    }
-)
+      initialState: {count:0},
+      reducer:{
+        Increment: (state)=>{state.count = state.count+1},
+        Decrement: (state)=>{state.count = state.count-1},
+        Reset: (state)=>{state.count =0}
+     }
+    })
