2b94f0d3d349223558033908341c5f2f11b7bc98
nikhilrautan <nikhilrautan1@gmail.com>
2026-07-15 09:58:46 +0530

    Update React Redux

---
+++ 04_React/15.Redux/Counting.js
@@ -0,0 +1,13 @@
+
+
+export default function Counting(){
+
+    return(
+        <>
+        <h1>Counter is {count}</h1>
+        <button>Increment</button>
+        <button>Decrement</button>
+        <button>Reset</button>
+        </>
+    )
+}
