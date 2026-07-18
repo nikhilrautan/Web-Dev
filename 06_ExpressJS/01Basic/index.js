c735ce1a4826c045b9f5f42959266cacad0f07c0
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-07-18 08:53:20 +0530

    Update index.js

--- 06_ExpressJS/01Basic/index.js
+++ 06_ExpressJS/01Basic/index.js
@@ -6,6 +6,14 @@ app.use((req,res)=>{
    res.send({"Name": Nikhil,"age":20});
 })
 
+app.use("/contact",(req,res)=>{
+  res.send("I am your contact page");
+})
+
+app.use("/about",(req,res)=>{
+   res.send("I am your about page");
+})
+
 app.listen(4000,()=>{
     console.log("Listening at port 4000");
 })
