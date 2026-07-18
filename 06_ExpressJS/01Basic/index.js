127ffefba693e31fa4e35a2c774ac2295ca3c91d
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-07-18 08:49:38 +0530

    Update index.js

--- 06_ExpressJS/01Basic/index.js
+++ 06_ExpressJS/01Basic/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express()
 
 app.use((req,res)=>{
-   res.send("Hello Coder Army");
+   res.send({"Name": Nikhil,"age":20});
 })
 
 app.listen(4000,()=>{
