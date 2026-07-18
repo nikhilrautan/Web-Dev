faf41f49a8338af0218ce1eda24853c9fb2e37db
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-07-18 10:53:04 +0530

    Update index.js of Day2

---
+++ 06_ExpressJS/02/frontend.js
@@ -0,0 +1,10 @@
+
+fetch("http://fkjbnsdfk.com")
+
+const response = await fetch('https://api.example.com/data',{
+  method: 'POST',
+  headers:{
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({name : 'John', age : 30})
+});
