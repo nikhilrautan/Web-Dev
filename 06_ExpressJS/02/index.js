e77dd0dd5dbaee59d62650cd735c23fa3ebe2081
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-07-18 10:13:58 +0530

    Update index.js of Day2

---
+++ 06_ExpressJS/02/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use("/user",(req,res)=>{
+
+    res.send("Hello Coder Army")
+})
+
+app.listen(4000,()=>{
+    console.log("Listening at port 4000");
+})
