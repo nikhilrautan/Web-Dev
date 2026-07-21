6d0526b42ac2511b4152ac541dcabf42fd8fab81
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-07-21 09:41:32 +0530

    Added new folder Midleware

---
+++ 06_ExpressJS/03Middleware/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+//CRUD 
+
+//Database : array
+
+const FoodMenu = [
+    {id:1, food:"Chowmein", category:"veg", price:500},
+    {id:2, food:"Butter Naan", category:"veg", price:100},
+    {id:3, food:"Chicken", category:"non-veg", price:1000},
+    {id:4, food:"Mutton", category:"non-veg", price:1500},
+    {id:5, food:"Momo", category:"veg", price:300},
+    {id:6, food:"Chai", category:"veg", price:50},
+    {id:7, food:"Rajma", category:"veg", price:300},
+    {id:8, food:"Roti", category:"veg", price:20},
+    {id:9, food:"Lolipop", category:"non-veg", price:700},
+    {id:10, food:"Kebab", category:"non-veg", price:400},
+    {id:11, food:"paneer", category:"veg", price:800},
+    {id:12, food:"Egg Curry", category:"non-veg", price:300},
+    {id:13, food:"salad", category:"veg", price:100},
+    {id:14, food:"shourma", category:"veg", price:300},
+    {id:15, food:"Butter Chicken", category:"non-veg", price:900},
+    {id:16, food:"Mushroom", category:"veg", price:700},
+]
+
+
+app.listen(300, ()=>{
+    console.log
+})
