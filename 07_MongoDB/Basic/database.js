cda82ea5634d40e53b52e7148a591fbb39dc2c2a
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-07-28 22:30:35 +0530

    Update

--- 07_MongoDB/Basic/database.js
+++ 07_MongoDB/Basic/database.js
@@ -1,1 +1,29 @@
+
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+
 const url ="mongodb+srv://admin:Nikhil7@cluster0.tlchu6y.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'myProject';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // The following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
