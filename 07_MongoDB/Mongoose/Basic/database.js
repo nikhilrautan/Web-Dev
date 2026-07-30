aa719f812ebd5c29ff56327ef17335162266b939
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-07-30 09:55:22 +0530

    Update Mongoose folder

--- 07_MongoDB/Mongoose/Basic/database.js
+++ 07_MongoDB/Mongoose/Basic/database.js
@@ -5,3 +5,8 @@ async function main(){
   // mongodb se connect ho gye
   await mongoose.connect("mongodb+srv://admin:Nikhil7@cluster0.tlchu6y.mongodb.net/");
 }
+
+
+main()
+.then(()=>console.log("Connect to DB"))
+.catch((err)=>console.log(err));
