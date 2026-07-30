361d3512d450322405f504cca1a322d0e898b82a
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-07-30 22:47:49 +0530

    Update

--- 06_ExpressJS/02/frontend.js
+++ 06_ExpressJS/02/frontend.js
@@ -1,23 +1,28 @@
-
 // Default get method
+// fetch by default sends a GET request, no need to specify method
 const response2 = await fetch('https://api.example.com/data')
-const data = response2.json();
+
+// converting response to JSON
+// note: .json() also returns a promise, so it needs await
+const data = await response2.json();  // <-- fixed: added missing await
 
 
 
+// POST request - used to send new data to server
 const response = await fetch('https://api.example.com/data', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json' // batana zaroori hai ki hum JSON bhej rahe hain
     },
-    body: JSON.stringify({ name: 'John', age: 30 })
+    body: JSON.stringify({ name: 'John', age: 30 }) // JS object ko JSON string me convert kar rahe hain
 });
 
 
+// PATCH request - used to update only some fields of existing data
 const response3 = await fetch('https://api.example.com/data', {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({age: 30 })
+    body: JSON.stringify({age: 30 }) // sirf age update ho rahi hai, baaki fields wahi rahenge
 });
