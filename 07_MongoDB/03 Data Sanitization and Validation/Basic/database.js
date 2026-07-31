25559e701f8a9c0ab6468ee9d568ce00ec8327e2
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-07-31 21:08:34 +0530

    Update index.js folder

--- 07_MongoDB/03 Data Sanitization and Validation/Basic/database.js
+++ 07_MongoDB/03 Data Sanitization and Validation/Basic/database.js
@@ -6,44 +6,10 @@ const mongoose = require('mongoose');
 async function main(){
 
   // mongodb se connect ho gye
-  await mongoose.connect("mongodb+srv://admin:Nikhil7@cluster0.tlchu6y.mongodb.net/Bookstore");
+  await mongoose.connect("mongodb+srv://admin:Nikhil7@cluster0.tlchu6y.mongodb.net/Instagram");
 
-  // code likhna shuru...
-  const userSchema = new Schema ({
-    name: String,
-    age:Number,
-    city:String,
-    gender:String
-  }) 
-
-  //Model lo create === Collection create krna (Table create krna)
-  // humne ek class create krii hai..
-
-  const User = mongoose.model("user",userSchema);
-
-//Document ko create kiya hai aur Oject ko create kiya hai..
- // 1 Step m poora krna 
-const user1 = new User({name:"Nikhil",age:20,city:"Chaukhutia",gender:"Male"});
-  await user1.save();
-
-  // 1 Step m poora krna
-  await User.create({name:"Mohan",age:30,city:"Pakistan"});
-
-  await User.insertMany([{name:"Yogesh",age:21},{age:25,gender:"Male"}]);
-
-  // find document
-  const ans = await User.find({});
-  console.log(ans);
-
-  // find document by particulatr field
-   const ans2 =await User.find({name:"Rohit"});
-   console.log(result);
   
 
 }
 
 module.exports = main;
-
-main()
-.then(()=>console.log("Connect to DB"))
-.catch((err)=>console.log(err));
--- 07_MongoDB/03 Data Sanitization and Validation/Basic/index.js
+++ 07_MongoDB/03 Data Sanitization and Validation/Basic/index.js
@@ -5,51 +5,11 @@ const User = require("./Models/users")
 
 app.use(express.json());
 
-// CRUD: Create Read Update Delete
+// post api
 
+app.post("/register", (req,res)=>{
 
-app.get("/info",async (req,res)=>{
-
-    const ans = await User.find({});
-    res.send(ans);
 })
-
-app.post("/info", async(req,res)=>{
-    
-    //nyii info insert krne k liye..
-
-    // const ans = new User(req.body);
-    // await ans.save();
-    
-
-    //JB BHI NETWORK CALL KRE TO (TRY,CATCH ) K ANDR KRE
-    try{ // kbhi koi error aaye to usko handle krne k liye
-    await User.create(req.body);
-    res.send("Succesfully Updated");
-    }
-    catch(err){
-        res.status(500).send(err);
-    }
-})
- 
-// DELETE KRNA HAI TO...
-app.delete("/info", async (req,res)=>{
-
-    // jiska bhi naam "Vishal" hai usko delete krdo..
-    await User.deleteOne({name:"Vishal"});
-    res.send("Deleted");
-})
-
-app.put("/info", async (req,res)=>{
-   
-    //UPDATE..
-    //1. updateOne
-    //2. updateMany
-    const result = await User.updateOne({ name: 'Mohan' }, { age: 40, city:"Bangladesh"});
-    // isme ye phla wala dhundhega ki 'Mohan' naam wala aadmi kha hai fir jaake uski value change krdo.
-    res.send("Updated Succesfully");
-})
-
 
 main()
 .then(async ()=>{
