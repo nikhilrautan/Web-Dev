b5c4c1718931e2d8fc4e78736af153db3d0a739c
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-08-06 21:22:45 +0530

    Updated

--- 07_MongoDB/05 API level Validation and Digital Signature/database.js
+++ 07_MongoDB/05 API level Validation and Digital Signature/database.js
@@ -8,7 +8,6 @@ async function main(){
   // mongodb se connect ho gye
   await mongoose.connect("mongodb+srv://admin:Nikhil7@cluster0.tlchu6y.mongodb.net/Instagram");
 
-  
 }
 
 module.exports = main;
