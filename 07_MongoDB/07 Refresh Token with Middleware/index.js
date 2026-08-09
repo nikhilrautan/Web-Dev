7a27c721e8fffa0812afbd655d5856cf7a8ce625
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-08-09 22:58:24 +0530

    Updated

--- 07_MongoDB/07 Refresh Token with Middleware/index.js
+++ 07_MongoDB/07 Refresh Token with Middleware/index.js
@@ -64,28 +64,8 @@ app.post("/login",async(req,res)=>{
 // FIND(isse hum kisi bhi user ko search kr skte hai jiski id hmare pass available hai..)
    app.get("/user", async(req,res)=>{
     try{
-        const {token}= req.cookies;
-        if(!token){
-            throw new Error("Token doesn't exist");
-        }
-         const payload= jwt.verify(token,"Nikhil@1347");
-        // console.log(payload);
-         
-         const {_id}= payload;
-
-         if(!_id){
-            throw new Error("Id is missing");
-         }
-        
-       const result = await User.findById(payload._id); 
-       res.send(result);
-// ab jb hum us user ki info ko get krenge to vha pr payload phle se present hoga
-//  to bina 'id' expose kre hum uski info ko return kra skte hai..
-     
-       if(!result){
-        throw new Error("User Doesn't Exist");
-       }
-}
+   res.send(result);
+  }
     catch(err){
    res.send("Error"+ err.message);
     }
---
+++ 07_MongoDB/07 Refresh Token with Middleware/middleware/userAuth.js
@@ -0,0 +1,31 @@
+
+const userAuth = async (req,res,next)=>{
+
+     try{
+            const {token}= req.cookies;
+            if(!token){
+                throw new Error("Token doesn't exist");
+            }
+             const payload= jwt.verify(token,"Nikhil@1347");
+            // console.log(payload);
+             
+             const {_id}= payload;
+    
+             if(!_id){
+                throw new Error("Id is missing");
+             }
+            
+           const result = await User.findById(payload._id); 
+           res.send(result);
+    // ab jb hum us user ki info ko get krenge to vha pr payload phle se present hoga
+    //  to bina 'id' expose kre hum uski info ko return kra skte hai..
+         
+           if(!result){
+            throw new Error("User Doesn't Exist");
+           }
+    }
+        catch(err){
+       res.send("Error: "+ err.message);
+        }
+    }
+    
