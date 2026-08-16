9452cdbddfd1dd98175140ac2f8d5d3da291268a
Nikhil Rautan <nikhilrautan1@gmail.com>
2026-08-17 00:26:55 +0530

    Updated

--- 07_MongoDB/11 Rate Limiter in LLD/middleware/rateLimiter.js
+++ 07_MongoDB/11 Rate Limiter in LLD/middleware/rateLimiter.js
@@ -2,10 +2,16 @@ const jwt = require('jsonwebtoken');
 const User = require("../Models/users");
 const redisClient = require('../config/redis');
 
-const rateLimiter = async (req,resizeBy,next){
+const rateLimiter = async (req,res,next){
 
     try{
-        const ip = req.ip;
+        const ip = req.ip;// isse hum client ka IP Address nikaal lenge
+        const count = await redisClient.incr(ip); // key ko ek se increase kr dega (aur agr bna nhi hai phle se to bna dega auruski value 1 hogi)
+        
+        if(count >60)
+        {
+            throw new Error("User Limit Exceeded");
+        }
     }
     catch(err){
         res.send("Error: "+err);
